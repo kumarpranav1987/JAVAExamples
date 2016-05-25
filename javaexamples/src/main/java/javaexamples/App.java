861a7ea8e10ec9404dc05f8a35474d1132e7736e
Pranav Kumar <pranav.kumar@iontrading.com>
2016-05-25 10:36:13 +0530

    Initial

---
+++ javaexamples/src/main/java/javaexamples/App.java
@@ -0,0 +1,13 @@
+package javaexamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
